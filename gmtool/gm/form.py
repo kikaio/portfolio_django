5b184a914fbddacd6ec2d96cb28306f11334791c
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-06-30 18:49:51 +0900

    gm login, register 로직.

--- gmtool/gm/form.py
+++ gmtool/gm/form.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 from django.contrib.auth.models import Group
-
+from django.contrib.auth import authenticate
 from gmtool.gm.model import Gm
 
 class GmCreateForm(forms.ModelForm):
@@ -77,3 +77,79 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(Gm, UserAdmin)
 admin.site.unregister(Group)
+
+
+
+class GmRegistForm(forms.ModelForm):
+
+    pw_confirm = forms.CharField(widget=forms.PasswordInput, max_length=100, )
+    password = forms.CharField(widget=forms.PasswordInput, max_length=100,)
+    password.widget.attrs.update({
+        "class": "form-control form-control-user",
+        "placeholder": "Password"
+    })
+
+    pw_confirm.widget.attrs.update({
+        "class": "form-control form-control-user",
+        "placeholder": "Repeat Password"
+    })
+
+    class Meta:
+        model = Gm
+        fields = ['email', 'password', 'pw_confirm']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({
+            "class" : "form-control form-control-user",
+            "placeholder": "Email Address"
+        })
+
+
+    def clean_pw_confirm(self):
+        pw = self.cleaned_data.get('password')
+        pw_confirm = self.cleaned_data.get('pw_confirm')
+        if pw != pw_confirm:
+            raise forms.ValidationError(_("password doesn't match"))
+        return pw_confirm
+
+    def save(self, commit=True):
+        email = self.cleaned_data['email']
+        pw = self.cleaned_data['password']
+
+        if Gm.objects.filter(email__exact=email).exists():
+            raise forms.ValidationError(_('Already exist gm id'))
+        Gm.objects.create_user(email, pw)
+    pass
+
+
+class GmLoginForm(forms.Form):
+
+    email = forms.EmailField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=100)
+
+    email.widget.attrs.update({
+        "class":"form-control form-control-user",
+        "aria - describedby": "emailHelp",
+        "placeholder":"Enter Email Address...",
+    })
+    password.widget.attrs.update({
+        "class":"form-control form-control-user",
+        "placeholder":"Password",
+    })
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not Gm.objects.filter(email__exact=email).exists():
+            raise forms.ValidationError(_('This Gm is not exist'))
+        return email
+
+    def login(self,  request):
+        email = self.cleaned_data['email']
+        pw = self.cleaned_data['password']
+        gm = authenticate(request, email=email, password=pw)
+        if gm is None:
+            print('login failed')
+            return False
+        else :
+            return True
--- gmtool/gm/view.py
+++ gmtool/gm/view.py
@@ -1,17 +1,43 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from gmtool.gm.form import *
+
 
 TEMPLATE_ROOT = 'gmtool/gm'
 
 def render_gm(req, template_name:str='', context:dict={}):
-    return render(req, f'{TEMPLATE_ROOT}/{template_name}')
+    return render(req, f'{TEMPLATE_ROOT}/{template_name}', context)
 
 
 def gm_login(req):
-    return render_gm(req, 'login.html')
+    context = {}
+    form =None
+    if req.method == 'POST':
+        form = GmLoginForm(req.POST)
+        if form.is_valid():
+            # 실제로 내부에선 로그인 처리.
+            if form.login(req):
+                return render(req, 'gmtool/index.html')
+            else:
+                return render_gm(req, 'login_failed.html')
+    elif req.method=='GET':
+        form = GmLoginForm()
+
+    context['form'] = form
+    return render_gm(req, 'login.html', context)
 
 def gm_register(req):
-    return render_gm(req, 'register.html')
+    form = None
+    context = {}
+    if req.method == 'POST':
+        form = GmRegistForm(req.POST)
+        if form.is_valid():
+            form.save()
+            context['next'] = reverse('gmtool:gm-login')
+    elif req.method == 'GET':
+        form = GmRegistForm()
+        context['form'] = form
+    return render_gm(req, 'register.html', context)
 
 def gm_change_pw(req):
     return render_gm(req, 'change_password.html')
