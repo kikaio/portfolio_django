6eeaeed20ab3d22f32b955e3a79f27a632f5029f
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-07-10 16:38:23 +0900

    OAuth 관련 생성 파일 추가.

---
+++ gmtool/oauth/enum.py
@@ -0,0 +1,18 @@
+from enum import *
+
+
+class PlatformType(IntEnum):
+    NONE = 0
+    JUST_EMAIL = 1
+    FACE_BOOK = 2
+    GOOGLE = 3
+    KAKAO = 4
+
+    @classmethod
+    def ToChoices(cls):
+        ret = {}
+        for ele in cls:
+            ret += {ele.name : ele.value }
+        return ret
+    pass
+
---
+++ gmtool/oauth/model.py
@@ -0,0 +1,37 @@
+from gmtool.oauth.enum import *
+from django.db import models
+
+class GmPlatform(models.Model):
+    """
+    OAuth 고려한 Gm 추가 정보를 저장.
+    """
+    gm = models.ForeignKey(
+        'GmUser',
+        on_delete=models.CASCADE,
+        related_name='Platform',
+        related_query_name='platform',
+        blank=False,
+    )
+
+    platform_type = models.IntegerField(
+        default=PlatformType.NONE,
+        choices=PlatformType.ToChoices(),
+        null=True,
+        blank=False,
+    )
+
+    access_token = models.CharField(
+        default='',
+        null=True,
+        blank=True,
+        max_length=100,
+    )
+
+    reflesh_token = models.CharField(
+        default='',
+        null=True,
+        blank=True,
+        max_length=100,
+    )
+
+    pass
