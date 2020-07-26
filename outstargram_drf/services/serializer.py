f465dc18bdef686a612cee68847abe4e11ac1cf6
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-07-26 19:25:33 +0900

    Gm, Author 관련 serialize 작업중.

---
+++ outstargram_drf/services/serializer.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from outstargram_drf.models import *
+
+
+class SerGm(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(read_only=True)
+    password = serializers.CharField(
+        read_only=True,
+        style = {
+            'input_type' : 'password'
+        }
+    )
+    is_active = serializers.BooleanField(default=True, )
+    is_superuser = serializers.BooleanField(default=False, )
+    is_locked = serializers.BooleanField(default=False, )
+
+    def create(self, validated_data):
+        email = validated_data.get('email', None)
+        pw = validated_data.get('pw', None)
+        if email is None or pw is None:
+            raise serializers.ValidationError("input is invalid")
+        new_user = User.object.create_user(email, pw)
+        return new_user
+
+    def update(self, instance, validated_data):
+        instance.password = validated_data.get('password', None)
+        if instance.password is None:
+            raise serializers.ValidationError('input is invalid')
+        instance.is_active = validated_data.get('is_active', True)
+        instance.is_superuser = validated_data.get('is_superuser', False)
+        instance.is_locked = validated_data.get('is_locked', False)
+        instance.save()
+        return instance
+
+
+class SerAuthor(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    user = serializers.RelatedField(read_only=True)
+    follow_cnt = serializers.IntegerField(read_only=True)
+    follower_cnt = serializers.IntegerField(read_only=True)
+    desc = serializers.CharField(default='', max_length=100)
+
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
+
+    def update(self, inst, validated_data):
+        inst.follow_cnt = validated_data.get('follow_cnt', inst.follow_cnt)
+        inst.follower_cnt = validated_data.get('follower_cnt', inst.follower_cnt)
+        inst.desc = validated_data.get('desc', inst.desc)
+        inst.save()
+        return inst
+    pass
+
--- outstargram_drf/sign/views.py
+++ outstargram_drf/sign/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from outstargram_drf.sign.serializer import *
+from outstargram_drf.services.serializer import *
+
+
+@csrf_exempt
+def gm_list(req):
+    if req.method == 'POST':
+        data = JSONParser().parse(req)
+        ser = SerGm(data = data)
+        if ser.is_valid():
+            ser.save()
+            return JsonResponse(ser.data, status=201)
+        return JsonResponse(ser.errors, status=400)
+    elif req.method == 'GET':
+        gms = User.objects.all()
+        ser = SerGm(gms, many=True)
+        return JsonResponse(ser.data, safe = False)
+
+def gm_detail(req, pk):
+    pass
+
+@csrf_exempt
+def author_list(req):
+    if req.method == 'POST':
+        pass
+    elif req.method == 'GET':
+        pass
+    else:
+        pass
+    pass
