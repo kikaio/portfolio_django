4684f732b33afdd82b209b205081fbbe4ec1b2df
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-07-27 20:11:21 +0900

    불필요 코드제거

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -179,7 +179,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
 }
