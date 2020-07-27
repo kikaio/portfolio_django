ccc85272bdcaad4ac3a53ed9725a9f7c3e2e2bf7
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-07-27 21:08:28 +0900

    api root 추가

--- outstargram_drf/views.py
+++ outstargram_drf/views.py
@@ -1,9 +1,24 @@
+
 from django.shortcuts import render
+
+from rest_framework.reverse import reverse
 
 from outstargram_drf.sign.views import *
 from outstargram_drf.services.views import *
 
 # Create your views here.
 
-def index(req):
-    return ''
+@api_view(['GET'])
+def index(req, format=None):
+    context = {}
+    context['gm_list'] = reverse(
+        'outstargram-drf:gm-list',
+        request=req,
+        format = format
+    )
+    context['author_list'] = reverse(
+        'outstargram-drf:author-list',
+        request=req,
+        format=format
+    )
+    return Response(context)
