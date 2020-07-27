962384ed1ca7c2f1c84f9ba7c97dfae532b94159
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-07-27 21:08:16 +0900

    drf pagination 추가

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -175,8 +175,7 @@ SESSION_SAVE_EVERY_REQUEST = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
