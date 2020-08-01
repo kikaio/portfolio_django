35f487e759bd35dd9d83759f9a614071287d619e
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-08-01 21:59:32 +0900

    user 일치시에만 DELETE 가능한 permission, Follower 해제 관련 permission 추가

--- outstargram_drf/services/perms.py
+++ outstargram_drf/services/perms.py
@@ -13,19 +13,30 @@ class IsPostOwnerOrReadOnly(permissions.BasePermission):
     pass
 
 
-class IsJustForUser(permissions.BasePermission):
+class IsDeleteJustForUser(permissions.BasePermission):
     """user 외엔 어떠한 작업도 불가."""
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         if request.user == obj.user:
+            if request.method == 'DELETE':
+                return True
+        return False
+    pass
+
+
+class IsJustForUserLikeCancel(permissions.BasePermission):
+    """like 한 user 외엔 어떠한 작업도 불가."""
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.like_user:
             return True
         return False
     pass
 
 
-class IsJustForLikeUser(permissions.BasePermission):
-    """like 한 user 외엔 어떠한 작업도 불가."""
+class IsFollowerCancel(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
-        if request.user == obj.like_user:
-            return False
+        if request.user == obj.follower and request.method =='DELETE':
+            return True
         return False
-    pass
