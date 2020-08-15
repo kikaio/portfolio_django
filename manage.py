5210e4d175a8b9c6c4538d05968748de7276d73d
gjdud <gjdud@DESKTOP-LJ2MCJH>
2020-08-15 18:54:22 +0900

    생각 잘못했던 코드 제거, 기본적으로 dev 세팅 사용하도록 수정.

--- manage.py
+++ manage.py
@@ -5,14 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.base')
-    for arg in sys.argv:
-        if arg == 'dev':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.base')
-        elif arg == 'test':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.test')
-        elif arg == 'live':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.live')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.dev')
 
     try:
         from django.core.management import execute_from_command_line
