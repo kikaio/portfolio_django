ca06487533761773d061deeb4b3a7b98b3171633
yonwook heo <gjduddnr5923@naver.com>
2021-04-19 17:46:49 +0900

    pythonanywhere에서 돌아가도록 수정중

---
+++ portfolio/wsgi/pythonanywhere.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+path = '/home/kikaio/'  # PythonAnywhere 계정으로 바꾸세요.
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolio.settings.pythonanywhere'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
